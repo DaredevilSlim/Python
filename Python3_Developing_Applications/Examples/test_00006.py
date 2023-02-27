c57170dd23993afc5db121c2e99158048b61a921
DaredevilSlim <DaredevilSlim@gmail.com>
2023-02-27 19:59:18 +0200

    first commit

---
+++ Python3_Developing_Applications/Examples/test_00006.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Пример использования функции input()
+name = input('Введите ваше имя: ')
+print('Привет, ', name)
+input('Нажмите <Enter> для закрытия окна')
---
+++ Python3_Developing_Applications/Examples/test_00007.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+# Получение данных из консоли
+arr = sys.argv[:]
+for n in arr:
+    print(n)

9b2309eebdff79aabfe31756e2f30469e20d4da8
DaredevilSlim <DaredevilSlim@gmail.com>
2023-03-25 21:17:40 +0200

    first commit

---
+++ Python3_Developing_Applications/Examples/test_00010.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import test_00008  # Подключаем файл test_00008.py
+print(test_00008.__doc__)
+print(test_00008.func.__doc__)
+input()
---
+++ Python3_Developing_Applications/Examples/test_00011.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import test_00008  # Подключаем файл test_00008.py
+print(dir(test_00008))
+input()

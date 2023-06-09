58b1c0b2737f6e9f10b815d64c94624d1ba62424
DaredevilSlim <DaredevilSlim@gmail.com>
2023-06-09 09:42:51 +0300

    first commit

---
+++ Python3_Developing_Applications/Examples/test_00014.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Проверка числа на четность
+x = int(input('Введите число: '))
+if x % 2 == 0:
+    print(x, '- четное число')
+else:
+    print(x, '- нечетное число')
+input()
---
+++ Python3_Developing_Applications/Examples/test_00016.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Вложенные инструкции
+print('''Kaкoй операционной системой вы пользуетесь?
+1 - Windows 11
+2 - Windows 10
+3 - Windows 8.1
+4 - Windows 8
+5 - Windows 7
+6 - Другая''')
+os = input('Введите число, соответствующее ответу: ')
+if os != '':
+    if os == '1':
+        print('Вы выбрали: Windows 11')
+    elif os == '2':
+        print('Вы выбрали: Windows 10')
+    elif os == 'З':
+        print('Bы выбрали: Windows 8.1')
+    elif os == '4':
+        print('Bы вы6рали: Windows 8')
+    elif os == '5':
+        print('Вы вы6рали: Windows 7')
+    elif os == '6':
+        print('Bы вы6рали: другая')
+    else:
+        print('Мы не смогли определить вашу операционную систему')
+else:
+    print('Bы не ввели число')
+input()

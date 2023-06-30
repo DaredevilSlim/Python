9b778374f8bb7943943635da8e4e352f9f83fce4
DaredevilSlim <DaredevilSlim@gmail.com>
2023-06-30 09:57:35 +0300

    first commit

--- Checkio/List but not the least/stock_profit.py
+++ Checkio/List but not the least/stock_profit.py
@@ -16,18 +16,17 @@ def stock_profit(stock: list[int]) -> int:
     if len(stock) > 1:
         if b < a:
             return stock_profit(stock[:a] if len(stock) - 1 == a else stock[b + 1:])
-        return max(stock) - min(stock)
+        return stock[b] - stock[a]
     return 0
 
 
-print(stock_profit([2, 3, 4, 5]))  # 3
-print(stock_profit([3, 1, 3, 4, 5, 1]))  # 4
-print(stock_profit([4, 3, 2, 1]))  # 0
+print(stock_profit([2, 3, 4, 5]))                    # 3
+print(stock_profit([3, 1, 3, 4, 5, 1]))              # 4
+print(stock_profit([4, 3, 2, 1]))                    # 0
 print(stock_profit([6, 2, 1, 2, 3, 2, 3, 4, 5, 4]))  # 4
-print(stock_profit([1, 1, 1, 2, 1, 1, 1]))  # 1
-print(stock_profit([4, 3, 2, 1, 2, 1, 2, 1]))  # 1
-print(stock_profit([1, 1, 1, 1]))  # 0
-print(stock_profit([22, 10, 4, 4, 1]))  # 0
-print(stock_profit([80, 70, 10, 3, 7]))  # 4
-print(stock_profit([60, 50, 51, 52, 40]))  # 2
-
+print(stock_profit([1, 1, 1, 2, 1, 1, 1]))           # 1
+print(stock_profit([4, 3, 2, 1, 2, 1, 2, 1]))        # 1
+print(stock_profit([1, 1, 1, 1]))                    # 0
+print(stock_profit([22, 10, 4, 4, 1]))               # 0
+print(stock_profit([80, 70, 10, 3, 7]))              # 4
+print(stock_profit([60, 50, 51, 52, 40]))            # 2

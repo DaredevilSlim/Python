5118683668db878991a002085ab5902bf114b2a2
DaredevilSlim <DaredevilSlim@gmail.com>
2023-07-17 22:58:17 +0300

    first commit

---
+++ Python3_Developing_Applications/Examples/test_00043.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from functools import reduce  # Подключаем из модуля functools содержащийся в нем объект reduce
+
+
+# Пример использования функции reduce()
+def func(x, y):
+    print('({0}, {1})'.format(x, y), end=' ')
+    return x + y
+
+
+arr = [1, 2, 3, 4, 5]
+summa = reduce(func, arr)
+# Последовательность: (1, 2) (3, 3) (6, 4) (10, 5)
+print(summa)  # Результат выполнения: 15
+summa = reduce(func, arr, 10)
+# Последовательность: (10, 1) (11, 2) (13, 3) (16, 4) (20, 5)
+print(summa)  # Результат выполнения: 25
+summa = reduce(func, [], 10)
+print(summa)  # Результат выполнения: 10

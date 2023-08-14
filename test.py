b3b57480df2ab3d94cef61de3e58f09c75589115
DaredevilSlim <DaredevilSlim@gmail.com>
2023-08-14 23:58:41 +0300

    first commit

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-print('Hello world')
-print('Strange world')
-print('Something went wrong')
+print("Hello world")
+print("Strange world")
+print("Something went wrong")

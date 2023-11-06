02f94cbf7293bb4a1ba72d85eba2d4f4a0d57233
DaredevilSlim <DaredevilSlim@gmail.com>
2023-11-06 20:47:21 +0200

    New commit

--- Checkio/Strings_and_Integers/all_permutations.py
+++ Checkio/Strings_and_Integers/all_permutations.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from collections.abc import Iterable
+
 # Given a string, return all possible permutations of its characters, sorted alphabetically.
 # Input: String (str).
 # Output: Iterable of strings (str).
@@ -8,11 +10,18 @@
 # puzzles and games like Scrabble where word combinations matter;
 # cryptography to test possible keys given a set of characters;
 # data analysis in genetics for possible gene combinations.
-from collections.abc import Iterable
+
+
+def count_permutation(s: str) -> int:
+    a = 1
+    for i in range(len(s), 1, -1):
+        a *= i
+    return a
 
 
 def string_permutations(s: str) -> Iterable[str]:
-    # your code here
+    count = count_permutation(s)
+    print(count)
     return []
 
 

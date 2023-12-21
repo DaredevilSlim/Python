be90b5f323c3a44567edfff145b5edd4bdbc10f8
DaredevilSlim <DaredevilSlim@gmail.com>
2023-12-21 22:55:07 +0200

    New commit

---
+++ Starting_Out_With_Python/Chapter01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Ключевые (зарезервированные) слова языка Python
+# and       break       elif        for         in          not         True
+# as        class       else        from        is          or          try
+# assert    continue    except      global      lambda      pass        while
+# async     def         False       if          None        raise       with
+# await     del         finally     import      nonlocal    return      yield

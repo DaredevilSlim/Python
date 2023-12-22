4e6bf41a3111ecd429dc6fc5360cd721abb957f0
DaredevilSlim <DaredevilSlim@gmail.com>
2023-12-22 23:42:09 +0200

    New commit

---
+++ Starting_Out_With_Python/Chapter02/variable_demo2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Создать две переменные: top_speed и distance.
+top_speed = 160
+distance = 300
+
+# Показать значения, на которые ссылаются переменные.
+print('Предельная скорость составляет')
+print(top_speed)
+print('Пройденное расстояние составляет')
+print(distance)

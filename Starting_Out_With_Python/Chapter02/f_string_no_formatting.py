e7c8b972819fca54cda06afe995c7017db9eabbc
DaredevilSlim <DaredevilSlim@gmail.com>
2023-12-23 23:48:03 +0200

    first commit

---
+++ Starting_Out_With_Python/Chapter02/f_string_no_formatting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Эта программа демонстрирует, как отображается число с плавающей точкой без форматирования.
+amount_due = 5000.0
+monthly_payment = amount_due / 12.0
+print(f'Ежемесячный платеж составляет {monthly_payment}.')
---
+++ Starting_Out_With_Python/Chapter02/f_string_rounding.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Эта программа демонстрирует округление числа с плавающей точкой.
+amount_due = 5000.0
+monthly_payment = amount_due / 12.0
+print(f'Ежемесячный платеж составляет {monthly_payment:.2f}.')

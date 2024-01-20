086af6a03b6f34444dd0d6786b2ad09b5f71bf7c
DaredevilSlim <DaredevilSlim@gmail.com>
2024-01-20 18:43:56 +0200

    first commit

---
+++ Starting_Out_With_Python/Chapter06/display_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Эта программа показывает содержимое файла.
+def main():
+    # Получить имя файла.
+    filename = input('Введите имя файла: ')
+    # Открыть файл.
+    infile = open(filename, 'r')
+    # Прочитать содержимое файла.
+    contents = infile.read()
+    # Показать содержимое файла.
+    print(contents)
+    # Закрыть файл.
+    infile.close()
+
+
+# Вызвать главную функцию.
+if __name__ == '__main__':
+    main()
---
+++ Starting_Out_With_Python/Chapter06/file_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Эта программа пишет три строки данных в файл.
+def main():
+    # Открыть файл с именем philosophers.txt.
+    outfile = open('philosophers.txt', 'w')
+    # Записать имена трех философов в файл.
+    outfile.write('Джон Локк\n')
+    outfile.write('Дэвид Хьюм\n')
+    outfile.write('Эдмунд Берк\n')
+    # Закрыть файл.
+    outfile.close()
+
+
+# Вызвать главную функцию.
+if __name__ == '__main__':
+    main()

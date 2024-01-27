1a7a8a448892025ddc6d10117f401bc9c18ea43a
daredevilslim <daredevilslim@gmail.com>
2024-01-27 23:03:43 +0200

    New commit

--- LeetCode/Easy/isomorphic_strings.py
+++ LeetCode/Easy/isomorphic_strings.py
@@ -21,12 +21,20 @@
 def is_isomorphic(s: str, t: str) -> bool:
     if s == t:
         return True
-    if len(set(s)) != len(set(t)):
-        return False
-    for i, j in zip(s, t):
-        if s.count(i) != t.count(j):
-            return False
-    return True
+    s_list = list(s[0])
+    t_list = list(t[0])
+    s_index = 0
+    t_index = 0
+    for i, j in zip(range(1, len(s)), range(1, len(t))):
+        if s[i] in s[:i]:
+            s_index += s[:i].rindex(s[i])
+        if s[i] != s_list[-1][-1]:
+            s_list.append(s[i])
+        if t[j] in t[:j]:
+            t_index += t[:j].rindex(t[j])
+        if t[j] != t_list[-1][-1]:
+            t_list.append(t[j])
+    return len(s_list) == len(t_list) and s_index == t_index
 
 
 print(is_isomorphic('a', 'a'))  # True
@@ -37,3 +45,5 @@ print(is_isomorphic('paper', 'title'))  # True
 print(is_isomorphic('bbbaaaba', 'aaabbbba'))  # False
 print(is_isomorphic('foo', 'bar'))  # False
 print(is_isomorphic('papap', 'titii'))  # False
+print(is_isomorphic('badc', 'baba'))  # False
+print(is_isomorphic('abcdefghijklmnopqrstuvwxyzva', 'abcdefghijklmnopqrstuvwxyzck'))  # False

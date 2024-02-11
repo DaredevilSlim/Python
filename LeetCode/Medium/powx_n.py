4164bd353933228506d7fd582ce2739a94501455
daredevilslim <daredevilslim@gmail.com>
2024-02-12 01:09:54 +0200

    New commit

--- LeetCode/Medium/powx_n.py
+++ LeetCode/Medium/powx_n.py
@@ -18,16 +18,36 @@
 # n is an integer.
 # Either x is not zero or n > 0.
 # -104 <= xn <= 104
+#     def recursion(num_x: float, pow_n: int) -> float:
+#         if num_x == 0:
+#             return 0
+#         if pow_n == 0:
+#             return 1
+#         res = recursion(num_x * num_x, pow_n // 2)
+#         return num_x * res if pow_n % 2 else res
+#     result = recursion(x, abs(n))
+#     return result if n >= 0 else 1 / result
 def my_pow(x: float, n: int) -> float:
-    def recursion(num_x: float, pow_n: int) -> float:
-        if num_x == 0:
-            return 0
-        if pow_n == 0:
+    if x == 0:
+        return 0
+    if n == 0:
+        return 1
+    if x == -1:
+        if n < 0:
             return 1
-        res = recursion(num_x * num_x, pow_n // 2)
-        return num_x * res if pow_n % 2 else res
-    result = recursion(x, abs(n))
-    return result if n >= 0 else 1 / result
+        return -1
+    pow_n = abs(n)
+    m = abs(n)
+    i = 1
+    num_x = x
+    while pow_n // 2:
+        pow_n = pow_n // 2
+        i *= 2
+        num_x *= num_x
+    m -= i
+    if pow_n % 2 and m <= 1000000:
+        num_x *= x ** m
+    return num_x if n >= 0 else 1 / num_x
 
 
 print(my_pow(2.00000, 10))  # 1024.00000
@@ -37,3 +57,5 @@ print(my_pow(0.00001, 2147483647))  # 0.00000
 print(my_pow(-0.82541, 12))  # 0.10001
 print(my_pow(1.00000, 2147483647))  # 1.00000
 print(my_pow(2.00000, -2147483647))  # 0.00000
+print(my_pow(1.00001, 123456))  # 3.43684
+print(my_pow(-1.00000, 2147483647))  # 1.00000

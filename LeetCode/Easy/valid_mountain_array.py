d1c034c655de51055da6559b7533124dae5957ab
daredevilslim <daredevilslim@gmail.com>
2024-02-28 23:37:42 +0200

    New commit

--- LeetCode/Easy/valid_mountain_array.py
+++ LeetCode/Easy/valid_mountain_array.py
@@ -20,9 +20,20 @@
 # 1 <= arr.length <= 104
 # 0 <= arr[i] <= 104
 def valid_mountain_array(arr: list[int]) -> bool:
-    pass
+    if len(arr) == 1 or arr[0] > arr[1]:
+        return False
+    i = 1
+    while i < len(arr) and arr[i-1] < arr[i]:
+        i += 1
+    if i == len(arr):
+        return False
+    while i < len(arr) and arr[i-1] > arr[i]:
+        i += 1
+    return i == len(arr)
 
 
+print(valid_mountain_array([2]))  # False
 print(valid_mountain_array([2, 1]))  # False
 print(valid_mountain_array([3, 5, 5]))  # False
 print(valid_mountain_array([0, 3, 2, 1]))  # True
+print(valid_mountain_array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))  # False

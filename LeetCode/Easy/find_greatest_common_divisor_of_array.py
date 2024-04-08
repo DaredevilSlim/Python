6fcc3581d87f0f09ff0b9d7ec0202cde113d8728
daredevilslim <daredevilslim@gmail.com>
2024-04-09 01:32:19 +0300

    New commit

---
+++ LeetCode/Easy/find_greatest_common_divisor_of_array.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+# The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+# Example 1:
+# Input: nums = [2, 5, 6, 9, 10]
+# Output: 2
+# Explanation:
+# The smallest number in nums is 2.
+# The largest number in nums is 10.
+# The greatest common divisor of 2 and 10 is 2.
+# Example 2:
+# Input: nums = [7, 5, 6, 8, 3]
+# Output: 1
+# Explanation:
+# The smallest number in nums is 3.
+# The largest number in nums is 8.
+# The greatest common divisor of 3 and 8 is 1.
+# Example 3:
+# Input: nums = [3, 3]
+# Output: 3
+# Explanation:
+# The smallest number in nums is 3.
+# The largest number in nums is 3.
+# The greatest common divisor of 3 and 3 is 3.
+# Constraints:
+# 2 <= nums.length <= 1000
+# 1 <= nums[i] <= 1000
+def find_gcd(nums: list[int]) -> int:
+    nums = sorted(nums)
+    i = nums[0]
+    while i > 0 and (nums[0] % i + nums[-1] % i) != 0:
+        i -= 1
+    return i
+
+
+print(find_gcd([2, 5, 6, 9, 10]))  # 2
+print(find_gcd([7, 5, 6, 8, 3]))  # 1
+print(find_gcd([3, 3]))  # 3
+print(find_gcd([6, 9, 10]))  # 2
